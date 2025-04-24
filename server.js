44e22331723332d256b46394f848ff58ab366ae1
raj singh <rajsinghast03@gmail.com>
2025-04-24 21:33:44 +0530

    changed backend url

--- server.js
+++ server.js
@@ -43,5 +43,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
+  console.log(
+    `Swagger docs at https://ideacraft-assignment.onrender.com/api-docs`
+  );
 });
--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000/api",
+        url: "https://ideacraft-assignment.onrender.com/api",
       },
     ],
     components: {
